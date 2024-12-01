62516027e6a8f0fb899354d5983d530bae73eeca
thinhnw <thinhnv0412@gmail.com>
2024-12-01 15:24:56 +0700

    Refactor

--- app/main.c
+++ app/main.c
@@ -14,13 +14,15 @@ int main() {
     char *command = strtok(input, " \n");
     if (command == NULL) {
       continue; 
-    } else if (strcmp(command, "exit") == 0) {
-      exit(0);
-    } else if (strcmp(command, "echo") == 0) {
-      printf("%s", input + strlen(command) + 1);
-    } else {
-      printf("%s: command not found\n", command);
     }
+    if (strcmp(command, "exit") == 0) {
+      exit(0);
+    }
+    if (strcmp(command, "echo") == 0) {
+      printf("%s", input + strlen(command) + 1);
+      continue;
+    }
+    printf("%s: command not found\n", command);
   }
   return 0;
 }
