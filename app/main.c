8f4202ddeb1bce6a26ac29c8609537f923233a4d
thinhnw <thinhnv0412@gmail.com>
2024-12-01 15:02:12 +0700

    Implement REPL

--- app/main.c
+++ app/main.c
@@ -2,14 +2,15 @@
 #include <string.h>
 
 int main() {
-  // Uncomment this block to pass the first stage
-  printf("$ ");
-  fflush(stdout);
+  while (1) {
+    printf("$ ");
+    fflush(stdout);
 
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
+    // Wait for user input
+    char input[100];
+    fgets(input, 100, stdin);
 
-  printf("%.*s: command not found\n", (int)(strlen(input) - 1), input);
+    printf("%.*s: command not found\n", (int)(strlen(input) - 1), input);
+  }
   return 0;
 }
