a0f4988ccac42c4371b2bcb568a3517ca8d75845
thinhnw <thinhnv0412@gmail.com>
2024-12-01 15:47:30 +0700

    Handle `type` builtin

--- app/main.c
+++ app/main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
+  const char *builtins[] = {"type", "exit", "echo"};
+  size_t num_builtins = sizeof(builtins) / sizeof(builtins[0]);
+
   while (1) {
     printf("$ ");
     fflush(stdout);
@@ -12,16 +15,30 @@ int main() {
     fgets(input, 100, stdin);
 
     char *command = strtok(input, " \n");
-    if (command == NULL) {
-      continue; 
+
+    if (command == NULL) continue;
+
+    if (strcmp(command, "type") == 0) {
+      command = strtok(NULL, " \n");
+      if (command == NULL) continue;
+      int found = 0;
+      for (size_t i = 0; i < num_builtins; i++)
+        if (strcmp(command, builtins[i]) == 0) {
+          printf("%s is a shell builtin\n", command);
+          found = 1;
+          break;
+        }
+      if (!found) printf("%s: command not found\n", command);
+      continue;
     }
-    if (strcmp(command, "exit") == 0) {
-      exit(0);
-    }
+
+    if (strcmp(command, "exit") == 0) exit(0);
+
     if (strcmp(command, "echo") == 0) {
       printf("%s", input + strlen(command) + 1);
       continue;
     }
+
     printf("%s: command not found\n", command);
   }
   return 0;
