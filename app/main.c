9f0ce97617e010fb34f4e99cc4fa2a88144eb484
thinhnw <thinhnv0412@gmail.com>
2024-12-04 13:05:25 +0700

    Refactor code

--- app/main.c
+++ app/main.c
@@ -5,9 +5,13 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#define PATH_MAX 4096
 
-int which(const char *cmd_name, char **full_path) {
+#define INPUT_MAX 100
+#define PATH_MAX 4096
+const char *builtins[] = {"type", "exit", "echo"};
+size_t num_builtins = sizeof(builtins) / sizeof(builtins[0]);
+
+int find_command_in_path(const char *cmd_name, char **full_path) {
   char *path = getenv("PATH");
   if (!path) return 1;
 
@@ -20,109 +24,114 @@ int which(const char *cmd_name, char **full_path) {
     if (access(buffer, X_OK) == 0) {
       *full_path = strdup(buffer);
       free(path_copy);
-      return 0;
+      return 1;
     }
     dir = strtok(NULL, ":");
   }
-  return 1;
+  free(path_copy);
+  return 0;
+}
+
+void handle_type(const char *cmd) {
+  cmd = strtok(NULL, " \n");
+  if (cmd == NULL) return;
+
+  // check if cmd is a builtin
+  for (size_t i = 0; i < num_builtins; i++)
+    if (strcmp(cmd, builtins[i]) == 0) {
+      printf("%s is a shell builtin\n", cmd);
+      return;
+    }
+
+  // check if cmd is in PATH
+  const char *PATH = getenv("PATH");
+  if (PATH == NULL) return;
+
+  char *path_copy = strdup(PATH);
+  char *dir = strtok(path_copy, ":");
+
+  while (dir != NULL) {
+    DIR *dp = opendir(dir);
+    if (dp == NULL) {
+      dir = strtok(NULL, ":");
+      continue;
+    }
+
+    struct dirent *entry;
+    while ((entry = readdir(dp)) != NULL) {
+      if (strcmp(entry->d_name, cmd) != 0) continue;
+      char full_path[1024];
+      snprintf(full_path, sizeof(full_path), "%s/%s", dir, entry->d_name);
+      if (access(full_path, X_OK) == 0) {
+        printf("%s is %s\n", cmd, full_path);
+        free(path_copy);
+        return;
+      }
+    }
+
+    dir = strtok(NULL, ":");
+  }
+
+  free(path_copy);
+  printf("%s: not found\n", cmd);
+}
+
+void exec_cmd(char *cmd) {
+  char *cmd_name = strtok(cmd, " \n");
+
+  if (cmd_name == NULL) return;
+
+  if (strcmp(cmd_name, "type") == 0) {
+    handle_type(strtok(NULL, " \n"));
+    return;
+  }
+
+  if (strcmp(cmd_name, "exit") == 0) exit(0);
+
+  if (strcmp(cmd_name, "echo") == 0) {
+    printf("%s", cmd + strlen(cmd_name) + 1);
+    return;
+  }
+  // handle external command
+  const char *PATH = getenv("PATH");
+  if (PATH == NULL) {
+    printf("%s: command not found\n", cmd_name);
+    return;
+  }
+  char *full_path;
+  if (find_command_in_path(cmd_name, &full_path)) {
+    char *args = cmd + strlen(cmd_name) + 1;
+    args[strlen(args) - 1] = '\0';
+    pid_t pid;
+    pid = fork();
+    if (pid < 0) {
+      perror("fork");
+      return;
+    }
+    // run command in child process
+    if (pid == 0) {
+      if (execl(full_path, cmd_name, args, NULL) == -1) perror("execl failed");
+    }
+    // wait for child process to finish
+    int pid_status;
+    if (waitpid(pid, &pid_status, 0) == -1) perror("waitpid");
+    free(full_path);
+    return;
+  }
+
+  printf("%s: command not found\n", cmd_name);
 }
 
 int main() {
-  const char *builtins[] = {"type", "exit", "echo"};
-  size_t num_builtins = sizeof(builtins) / sizeof(builtins[0]);
-
-  const char *PATH = getenv("PATH");
-
   while (1) {
     printf("$ ");
     fflush(stdout);
 
     // Wait for user input
-    char input[100];
-    fgets(input, 100, stdin);
+    char input[INPUT_MAX];
+    fgets(input, INPUT_MAX, stdin);
 
-    char *command = strtok(input, " \n");
-
-    if (command == NULL) continue;
-
-    if (strcmp(command, "type") == 0) {
-      command = strtok(NULL, " \n");
-      if (command == NULL) continue;
-
-      int found = 0;
-
-      for (size_t i = 0; i < num_builtins; i++)
-        if (strcmp(command, builtins[i]) == 0) {
-          printf("%s is a shell builtin\n", command);
-          found = 1;
-          break;
-        }
-
-      if (found || PATH == NULL) continue;
-
-      char *path_copy = strdup(PATH);
-      char *dir = strtok(path_copy, ":");
-
-      while (dir != NULL) {
-        DIR *dp = opendir(dir);
-        if (dp == NULL) {
-          dir = strtok(NULL, ":");
-          continue;
-        }
-
-        struct dirent *entry;
-        while ((entry = readdir(dp)) != NULL) {
-          if (strcmp(entry->d_name, command) != 0) continue;
-          char full_path[1024];
-          snprintf(full_path, sizeof(full_path), "%s/%s", dir, entry->d_name);
-          if (access(full_path, X_OK) == 0) {
-            printf("%s is %s\n", command, full_path);
-            found = 1;
-            break;
-          }
-        }
-
-        dir = strtok(NULL, ":");
-      }
-
-      free(path_copy);
-
-      if (!found) printf("%s: not found\n", command);
-      continue;
-    }
-
-    if (strcmp(command, "exit") == 0) exit(0);
-
-    if (strcmp(command, "echo") == 0) {
-      printf("%s", input + strlen(command) + 1);
-      continue;
-    }
-
-    if (PATH != NULL) {
-      char *full_path;
-      if (which(command, &full_path) == 0) {
-        char *args = input + strlen(command) + 1;
-        args[strlen(args) - 1] = '\0';
-        pid_t pid;
-        pid = fork();
-        if (pid < 0) {
-          perror("fork");
-          continue;
-        }
-        if (pid == 0) {
-          // run command in child process
-          if (execl(full_path, command, args, NULL) == -1)
-            perror("execl failed");
-        }
-        // wait for child process to finish
-        int pid_status;
-        if (waitpid(pid, &pid_status, 0) == -1) perror("waitpid");
-        free(full_path);
-        continue;
-      }
-    }
-
-    printf("%s: command not found\n", command);
+    exec_cmd(input);
   }
   return 0;
 }
